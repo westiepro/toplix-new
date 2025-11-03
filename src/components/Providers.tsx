ac11e3569c9390763bdb8060134446eae6bfbbfd
westiepro <71818595+westiepro@users.noreply.github.com>
2025-11-03 17:29:54 +0000

    Remove redundant LanguageProvider from Providers - already in layout

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,19 +2,16 @@
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import { FavoritesProvider } from "@/contexts/FavoritesContext";
-import { LanguageProvider } from "@/contexts/LanguageContext";
 import { Toaster } from "@/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<LanguageProvider>
-			<AuthProvider>
-				<FavoritesProvider>
-					{children}
-					<Toaster position="bottom-right" />
-				</FavoritesProvider>
-			</AuthProvider>
-		</LanguageProvider>
+		<AuthProvider>
+			<FavoritesProvider>
+				{children}
+				<Toaster position="bottom-right" />
+			</FavoritesProvider>
+		</AuthProvider>
 	);
 }
 
